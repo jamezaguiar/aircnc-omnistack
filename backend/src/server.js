df86b6240dbd3eb3e82054fbb1c411c571e0a97a
jamezaguiar <jamersonaguiar@alu.ufc.br>
2019-10-02 14:08:13 -0300

    Backend adjusts

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
 require("dotenv/config");
 const routes = require("./routes");
 const app = express();
@@ -9,6 +11,8 @@ mongoose.connect(process.env.DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
+app.use(cors());
 app.use(express.json());
+app.use("/files", express.static(path.resolve(__dirname, "../uploads")));
 app.use(routes);
 app.listen(3333);
